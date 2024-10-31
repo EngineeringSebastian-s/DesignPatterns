afe9dc0571f7ffa50a232519fc6de82b5220a5c4
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2024-10-31 10:07:30 -0500

    config space for index

--- js/Singleton.js
+++ js/Singleton.js
@@ -1,20 +1,37 @@
-// js/Singleton.js
-class Singleton {
-    constructor() {
-        if (Singleton.instance) {
-            return Singleton.instance;
-        }
-        Singleton.instance = this;
-        // Inicialización adicional
+/**
+ * ¿Cómo implementar Singleton?
+ * 1. Hacer privado el constructor
+ * 2. Crear un metodo estatico que llame al constructor privado
+ *    y guarde la instancia en una variable estatica
+ */
+
+class Singleton{
+
+    static instance = undefined; //Variable estatica que guarda la instancia
+
+    constructor(){
+        this.connection = this.connection; //Se crea una conexión para simular
     }
 
-    log() {
-        console.log('Instancia de Singleton');
+    static getInstance(connection){
+        if(!Singleton.instance){ //Si aun no se ha creado una instancia entonces que la cree
+            Singleton.instance = new Singleton(connection);
+        }
+        return Singleton.instance; //Si ya esta creada que la retorne
     }
 }
 
-// Exporta la función al contexto global
-window.singleton = function() {
-    const instance = new Singleton();
-    instance.log();
-};
+function appSingleton(){
+    const singleton1 = Singleton.getInstance('conexion1');
+    const singleton2 = Singleton.getInstance('conexion2');
+    const singleton3 = Singleton.getInstance('conexion3');
+
+    if(singleton1 === singleton2 && singleton2 === singleton3){
+        console.log('Singleton funciona ya que la conexión 1, conexion 2 y conexión 3 apunta a la misma instancia de objeto');
+        console.log(singleton1 === singleton2 && singleton2 === singleton3);
+    }
+    return('Error');
+    
+}
+
+appSingleton();
