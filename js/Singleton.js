ec1ce92c737d9cc49cc4550f66afef96110265a8
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2024-10-31 10:17:18 -0500

    create singleton

--- js/Singleton.js
+++ js/Singleton.js
@@ -21,17 +21,15 @@ class Singleton{
     }
 }
 
-export function appSingleton(){
+function appSingleton(){
     const singleton1 = Singleton.getInstance('conexion1');
     const singleton2 = Singleton.getInstance('conexion2');
     const singleton3 = Singleton.getInstance('conexion3');
 
     if(singleton1 === singleton2 && singleton2 === singleton3){
         console.log('Singleton funciona ya que la conexión 1, conexion 2 y conexión 3 apunta a la misma instancia de objeto');
-        console.log(singleton1 === singleton2 && singleton2 === singleton3);
+        console.log('¿Singleton1, Singleton2, Singleton3 son iguales?:' + singleton1 === singleton2 && singleton2 === singleton3);
     }
     return('Error');
     
 }
-
-appSingleton();
